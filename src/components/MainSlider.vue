6d495482bfc8352ee30807bad240074b6d079e28
pelevin-d-y <natpuot1992@gmail.com>
2018-04-23 22:51:16 +0300

    footer left

--- src/components/MainSlider.vue
+++ src/components/MainSlider.vue
@@ -21,19 +21,17 @@
     </div>
     <div class="main-slider__container">
       <div class="main-text">
-          <div class="empty-block">
+        <div class="main-text__big">
+          <div class="main-text__big-item">
+          <img src="../assets/images/drift.svg" class="main-text__big-item-drift">
           </div>
-          <div class="main-text__big">
-            <div class="main-text__big-item">
-            <img src="../assets/images/drift.svg" class="main-text__big-item-drift">
-            </div>
-            <div class="main-text__big-item">
-            <img src="../assets/images/is-my.svg" class="main-text__big-item-my">
-            </div>
-            <div class="main-text__big-item">
-            <img src="../assets/images/therapy.svg" class="main-text__big-item-therapy">
-            </div>
+          <div class="main-text__big-item">
+          <img src="../assets/images/is-my.svg" class="main-text__big-item-my">
           </div>
+          <div class="main-text__big-item">
+          <img src="../assets/images/therapy.svg" class="main-text__big-item-therapy">
+          </div>
+        </div>
         <div class="desc-wrapper">
           <div class="toggle">
             <div class="toggle__min-text">
@@ -58,7 +56,20 @@
       </div>
     </div>
     <footer class="footer">
-
+      <div class="share">
+        Share
+      </div>
+      <div class="socials">
+        <div class="socials-item socials-youtube">
+          <img src="../assets/images/youtube.svg">
+        </div>
+        <div class="socials-item socials-youtube">
+          <img src="../assets/images/inst.svg">
+        </div>
+        <div class="socials-item socials-youtube">
+          <img src="../assets/images/facebook.svg">
+        </div>
+      </div>
     </footer>
   </div>
 </template>
@@ -212,5 +223,32 @@
     color: #ffffff;
 
     background: #683FFF;
+  }
+
+  .footer {
+    position: relative;
+    z-index: 1;
+
+    display: flex;
+    flex-flow: row wrap;
+
+    margin-top: 92px;
+  }
+
+  .share {
+    width: 37.3%;;
+
+    font-size: 20px;
+    color: #fff;
+    text-transform: uppercase;
+  }
+
+  .socials {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: space-between;
+
+    max-width: 100px;
+    width: 100%;
   }
 </style>
